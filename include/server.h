0b3050492a22462cc569d2b04308c7a69f749cdf
Rihards Skuja <rssk@protonmail.com>
2017-12-27 01:33:43 +0200

    Allow client handling access only in server

--- include/server.h
+++ include/server.h
@@ -4,7 +4,7 @@
 extern void server_mode(const unsigned long port);
 
 static int create_sock(unsigned long port);
-void handle_clients(int sock);
+static void handle_clients(int sock);
 static int wait_for_client(int sock);
 
 #endif /* SERVER_H */
